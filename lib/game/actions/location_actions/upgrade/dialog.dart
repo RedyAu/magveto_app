cbd438052214469409df5c5880fd71ff4a935861
Fodor Benedek <benedek@fodor.pro>
2023-03-14 09:23:24 +0100

    upgrade location dialog

--- lib/game/actions/location_actions/upgrade/dialog.dart
+++ lib/game/actions/location_actions/upgrade/dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:magveto_app/graphics/index.dart';
 import 'package:provider/provider.dart';
 
 import '../../../../logic/index.dart';
@@ -15,14 +16,64 @@ class UpgradeLocationDialog extends StatefulWidget {
 class _UpgradeLocationDialogState extends State<UpgradeLocationDialog> {
   @override
   Widget build(BuildContext context) {
-    return Consumer<GameProvider>(
-      builder: (context, game, child) {
-        return ActionDialog(
-            heroTag: "upgrade",
-            title: "Fejlesztés",
-            icon: Icon(Icons.upgrade),
-            child: Center(child: Text("UpgradeLocationDialog")));
-      }
-    );
+    return Consumer<GameProvider>(builder: (context, game, child) {
+      return ActionDialog(
+          heroTag: "upgrade",
+          title:
+              game.characterInPlay.currentLocation!.type == LocationType.outpost
+                  ? "Gyülekezetplántálás"
+                  : "Templomépítés",
+          icon: Icon(Icons.upgrade),
+          child: Column(
+            children: [
+              Image.asset(
+                game.characterInPlay.currentLocation!.type ==
+                        LocationType.outpost
+                    ? "assets/button/outpost/${game.teamInPlay.id}.png"
+                    : "assets/button/community/${game.teamInPlay.id}.png",
+                height: 100,
+                filterQuality: FilterQuality.medium,
+              ),
+              SizedBox(height: 20),
+              ActionSegmentTitle(game.characterInPlay.currentLocation!.type ==
+                      LocationType.outpost
+                  ? "Három föld váltságot nyert, megalapíthatod a gyülekezetet!"
+                  : "A gyülekezetedben elindultak a szolgálatok, felépítheted a templomot!"),
+              ActionSegmentTitle(
+                "Már csak egy áldáscsillagot kell beadnod.",
+                subtitle: true,
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                  height: 65,
+                  child: game.characterInPlay.inventory!
+                      .getCompareWithBlessingWidgetList(Inventory(blessing: 1))
+                      .first),
+              SizedBox(height: 20),
+              FilledButton(
+                onPressed: game.characterInPlay.inventory!
+                        .canTake(Inventory(blessing: 1))
+                    ? () {
+                        game.characterInPlay.inventory!
+                            .take(Inventory(blessing: 1));
+                        game.characterInPlay.currentLocation!.type ==
+                                LocationType.outpost
+                            ? game.characterInPlay.currentLocation!.type =
+                                LocationType.community
+                            : game.characterInPlay.currentLocation!.type =
+                                LocationType.church;
+                        game.notify();
+                        Navigator.pop(context);
+                      }
+                    : null,
+                child: Text(
+                  "Mehet!",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              SizedBox(height: 10),
+            ],
+          ));
+    });
   }
 }
